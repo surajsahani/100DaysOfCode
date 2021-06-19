994db16106a19fcce0a9751feaf2dbb42f66cf44
suraj <surajkumarsahani1997@gmail.com>
2021-06-19 16:12:49 +0530

    Day 6

--- ExtractthePhoneNumber.java
+++ ExtractthePhoneNumber.java
@@ -1,27 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
-class ExtractthePhoneNumber
-{
-
+class ExtractthePhoneNumber {
     public static void main(String[] args)throws IOException
     {
-            BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-            int t = Integer.parseInt(read.readLine());
-            while(t-- > 0)
-            {
-                String S = read.readLine();
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-                Solution ob = new Solution();
-                System.out.println(ob.phoneNumber(S));
+            int t = Integer.parseInt(in.readLine());
+            while(t-- > 0) {
+                String text = in.readLine();
+
+                SolutionA ob = new SolutionA();
+                List<Integer> ans = new ArrayList<Integer>();
+
+                ans = ob.sentenceWord(text);
+                System.out.println(ans.get(0)+" "+ans.get(1));
             }
         }
     }
-
-    class Solution {
-      static String phoneNumber(String S)
-      {
-
-      }
+class SolutionA{
+    static List<Integer> sentenceWord(String text){
+        // code here
+        String[] word=text.split(" ");
+        int i=0,count=0;
+        char[] sen=text.toCharArray();
+        while(i<sen.length-1){
+            if((sen[i]=='.' || sen[i]=='?' || sen[i]=='!') && sen[i+1]==' '){
+                count++;
+            }
+            i++;
+        }
+        count++;
+        List<Integer> result = new Stack<Integer>();
+        result.add(count);
+        result.add(word.length);
+        return result;
+    }
     }
