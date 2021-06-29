24cd54c1ab1d2e70ce5556715a56b55144c47b1c
suraj <surajkumarsahani1997@gmail.com>
2021-06-29 08:28:16 +0530

    Day: list

---
+++ ListDemo.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args)
+    {
+
+        List<Integer> l1
+                = new ArrayList<Integer>();
+
+        l1.add(0,1);
+        l1.add(1,2);
+        System.out.println(l1);
+        List<Integer> l2
+                = new ArrayList<Integer>();
+
+        l2.add(1);
+        l2.add(2);
+        l2.add(3);
+
+        l1.addAll(1,l2);
+        System.out.println(l1);
+
+        l1.remove(1);
+        System.out.println(l1);
+
+        System.out.println(l1.get(3));
+
+        l1.set(0,5);
+        System.out.println(l1);
+    }
+}
